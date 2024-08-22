3583fe4eaa1d4d14379fd1b2325a11ac3cae6ec0
Joel De Jesus <joel@noncepad.com>
2024-08-22 14:10:43 +0900

    added client code

--- README.md
+++ README.md
@@ -1,11 +1,36 @@
 # solana-tx-processor
 
-This is a gRPC <--> JSON RPC translating proxy to use in front of a validator.
+This is a:
+
+1. gRPC <-- JSON RPC translating proxy to use in front of a validator and behind a pipeline reverse proxy
+2. a JSON RPC -> gRPC translating proxy to use in front of a bidder proxy daemon.
+
+## Install
+
+```bash
+go install github.com/noncepad/solana-tx-processor@main
+```
 
 ## Run
 
+### Validator Wrapper
+
+Pipelines run this program in front of the validator.
+
 ```bash
-go build -o ./txproc github.com/noncepad/solana-tx-processor
-RPC_URL=http://localhost:18899 WS_URL=ws://localhost:18900 ./txproc 15 tcp://:50051
+RPC_URL=http://localhost:18899 WS_URL=ws://localhost:18900 solana-tx-processor server 15 tcp://:50051
 ```
 
+* 15 is the worker count
+* `tcp://:50051` is the address on which the grpc server will listen
+
+### Json Rpc Endpoint
+
+Bidders run this program in front of their proxy daemon.
+
+```bash
+solana-tx-processor client tcp://:8899 unix:///tmp/my-proxy-daemon.sock
+```
+
+* `tcp://:8899` is the listening address for the JSON RPC endpoint that only accepts `sendTransaction` calls.
+* `unix:///tmp/my-proxy-daemon.sock` is the address that the proxy daemon is listening on
--- main.go
+++ main.go
@@ -11,27 +11,42 @@ import (
 	"strings"
 	"syscall"
 
+	"github.com/noncepad/solana-tx-processor/proxy"
 	"github.com/noncepad/solana-tx-processor/server"
 	log "github.com/sirupsen/logrus"
 	"google.golang.org/grpc"
 )
 
-// ./txproc 15 tcp://:50051
-func main() {
+const (
+	CMD_SERVER = "server"
+	CMD_CLIENT = "client"
+)
+
+func run_client(ctx context.Context, args []string) error {
+	if len(args) < 2 {
+		return errors.New("not enough args: need listen url and proxy url")
+	}
+	config := new(proxy.Configuration)
+	config.ListenUrl = args[0]
+	config.ProxyUrl = args[1]
+	return proxy.Run(ctx, config)
+}
+
+func run_server(ctx context.Context, args []string) error {
 	var present bool
 	var err error
 	config := new(server.Configuration)
-	if len(os.Args) < 3 {
-		panic(errors.New("no worker count and listen url specified"))
+	if len(args) < 2 {
+		return errors.New("no worker count and listen url specified")
 	}
 
 	config.WorkerCount, err = strconv.Atoi(os.Args[1])
 	if err != nil {
-		panic(err)
+		return err
 	}
 	log.SetLevel(log.DebugLevel)
 	var l net.Listener
-	url := os.Args[2]
+	url := args[1]
 	if strings.HasPrefix(url, "unix://") {
 		l, err = net.Listen("unix", strings.TrimPrefix(url, "unix://"))
 	} else if strings.HasPrefix(url, "tcp://") {
@@ -40,32 +55,53 @@ func main() {
 		err = fmt.Errorf("unknown url protocol %s", url)
 	}
 	if err != nil {
-		panic(err)
+		return err
 	}
 	config.Rpc, present = os.LookupEnv("RPC_URL")
 	if !present {
-		panic(errors.New("no rpc url specified"))
+		return errors.New("no rpc url specified")
 	}
 	config.Ws, present = os.LookupEnv("WS_URL")
 	if !present {
-		panic(errors.New("no ws url specified"))
+		return errors.New("no ws url specified")
 	}
+	s := grpc.NewServer()
+
+	err = server.Run(ctx, config, s)
+	if err != nil {
+		return err
+	}
+	go loopClose(ctx, l, s)
+	return s.Serve(l)
+}
+
+// Do ./txproc server 15 tcp://:50051
+// Or ./txproc client tcp://127.0.0.1:8899 tcp://localhost:50051
+func main() {
+	if len(os.Args) < 1 {
+		panic(errors.New("no argument specified"))
+	}
+
 	signalC := make(chan os.Signal, 1)
 	signal.Notify(signalC, syscall.SIGTERM, syscall.SIGINT)
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 	go loopSignal(ctx, cancel, signalC)
-	s := grpc.NewServer()
-
-	err = server.Run(ctx, config, s)
-	if err != nil {
-		panic(err)
-
+	var err error
+	args := os.Args[1:]
+	switch args[0] {
+	case CMD_CLIENT:
+		err = run_client(ctx, args)
+	case CMD_SERVER:
+		err = run_server(ctx, args)
+	default:
+		err = fmt.Errorf("unknown command %s", args[0])
 	}
-	go loopClose(ctx, l, s)
-	err = s.Serve(l)
 	if err != nil {
-		panic(err)
+		fmt.Printf("failed: %s", err)
+		os.Exit(1)
+	} else {
+		os.Exit(0)
 	}
 }
 
